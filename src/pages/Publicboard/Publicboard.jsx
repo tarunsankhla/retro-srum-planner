0c6960a611046aa5a413e992aef63823c350e479
PuneetP16 <patelpunit95@gmail.com>
2022-05-08 03:11:25 +0530

    feat: add feedback into list

--- src/pages/Publicboard/Publicboard.jsx
+++ src/pages/Publicboard/Publicboard.jsx
@@ -1,5 +1,5 @@
 import "./Publicboard.css";
-import { NewComment } from "components";
+import { AddFeedback, NewComment } from "components";
 import { CommentCard } from "components";
 import { firestore } from "firebase.config";
 import { useParams } from "react-router";
@@ -10,6 +10,12 @@ export const Publicboard = () => {
 	const { userId, projectId } = useParams();
 
 	console.log({ userId, projectId }, useParams());
+	const [isModal, setIsModal] = useState(false);
+	const [columnName, setColumnName] = useState("");
+
+	const toggleModal = () => {
+		setIsModal((isModal) => !isModal);
+	};
 
 	const [project, setProject] = useState({});
 
@@ -18,10 +24,13 @@ export const Publicboard = () => {
 		try {
 			const usersList = await getDoc(userRef);
 
-			const project = Object.values(usersList.data()).find(
-				(project) => project.id === projectId
+			const [projectKey, project] = Object.entries(usersList.data()).find(
+				([key, value]) => value.id === projectId
 			);
+
+			project.key = projectKey;
 			setProject(project);
+			console.log({ project });
 		} catch (error) {
 			console.log(error);
 		}
@@ -33,6 +42,14 @@ export const Publicboard = () => {
 
 	return (
 		<div className="publicboard-wrapper">
+			{isModal ? (
+				<AddFeedback
+					toggleModal={toggleModal}
+					columnName={columnName}
+					userId={userId}
+					project={project}
+				/>
+			) : null}
 			<div className=" flex-row-center flex-justify-space-between pd-point8-lr mg-point6-bot">
 				<h1 className="title-lg">{project?.title}</h1>
 				<select className="publicboard-sorting">
@@ -44,7 +61,11 @@ export const Publicboard = () => {
 			<div className="publicboard-body">
 				<div className="publicboard-wentwell">
 					<h1 className="publicboard-title">{project?.column1?.name}</h1>
-					<NewComment />
+					<NewComment
+						toggleModal={toggleModal}
+						columnTitle={project?.column1?.name}
+						setColumnName={setColumnName}
+					/>
 					<div>
 						<CommentCard color="went-well" />
 						<CommentCard color="went-well" />
@@ -52,7 +73,11 @@ export const Publicboard = () => {
 				</div>
 				<div className="publicboard-improve">
 					<h1 className="publicboard-title">{project?.column2?.name}</h1>
-					<NewComment />
+					<NewComment
+						toggleModal={toggleModal}
+						columnTitle={project?.column2?.name}
+						setColumnName={setColumnName}
+					/>
 					<div>
 						<CommentCard color="improve" />
 						<CommentCard color="improve" />
@@ -60,7 +85,11 @@ export const Publicboard = () => {
 				</div>
 				<div className="publicboard-actions">
 					<h1 className="publicboard-title">{project?.column3?.name}</h1>
-					<NewComment />
+					<NewComment
+						toggleModal={toggleModal}
+						columnTitle={project?.column3?.name}
+						setColumnName={setColumnName}
+					/>
 					<div>
 						<CommentCard color="action" />
 						<CommentCard color="action" />
