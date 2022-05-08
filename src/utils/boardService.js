f7e2e2a2544de5ede17d07673cc604852eef5126
PuneetP16 <patelpunit95@gmail.com>
2022-05-09 05:25:05 +0530

    update: refactor

--- src/utils/boardService.js
+++ src/utils/boardService.js
@@ -157,7 +157,7 @@ function deleteProject(userId, projectId) {
 			Alert("error", err.message);
 		});
 
-	Alert("info", "Dashboard Deleted!!");
+	Alert("info", "Project Deleted!!");
 }
 
 function cloneProject(boardObj, userId, dashboard) {
