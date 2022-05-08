badcad4a142a8e49bd2f2401610161233e3283b1
PuneetP16 <patelpunit95@gmail.com>
2022-05-09 05:23:45 +0530

    add: notfound page

--- src/App.js
+++ src/App.js
@@ -3,42 +3,44 @@ import "./App.css";
 import { ROUTES } from "./utils/routes";
 import { Suspense } from "react";
 import Main from "./Main";
-import { Publicboard, HomePage, MainDashboard } from "pages";
+import { Publicboard, HomePage, MainDashboard, NotFound } from "pages";
 import { LoginPage } from "pages/LoginPage/LoginPage";
 import { SignUpPage } from "pages/SignUpPage/SignUpPage";
 import RequiredAuth from "components/common/privateroute/RequireAuth";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route element={<Main />}>
-          <Route
-            path={ROUTES.ROUTE_PATH_LandingPage}
-            element={
-              <Suspense fallback={<h1>Loading Home...</h1>}>
-                <HomePage />
-              </Suspense>
-            }
-          />
-          <Route path={ROUTES.ROUTE_PATH_LoginPage} element={<LoginPage />} />
-          <Route path={ROUTES.ROUTE_PATH_SignupPage} element={<SignUpPage />} />
-          <Route
-            path={ROUTES.ROUTE_PATH_PublicDashboardPage}
-            element={<Publicboard />}
-          />
-          <Route
-            path={ROUTES.ROUTE_PATH_UserDashboaedPage}
-            element={
-              <RequiredAuth>
-                <MainDashboard />
-              </RequiredAuth>
-            }
-          />
-        </Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route element={<Main />}>
+					<Route
+						path={ROUTES.ROUTE_PATH_LandingPage}
+						element={
+							<Suspense fallback={<h1>Loading Home...</h1>}>
+								<HomePage />
+							</Suspense>
+						}
+					/>
+					<Route path={ROUTES.ROUTE_PATH_LoginPage} element={<LoginPage />} />
+					<Route path={ROUTES.ROUTE_PATH_SignupPage} element={<SignUpPage />} />
+					<Route
+						path={ROUTES.ROUTE_PATH_PublicDashboardPage}
+						element={<Publicboard />}
+					/>
+					<Route
+						path={ROUTES.ROUTE_PATH_UserDashboaedPage}
+						element={
+							<RequiredAuth>
+								<MainDashboard />
+							</RequiredAuth>
+						}
+					/>
+				</Route>
+				<Route path="/404" element={<NotFound />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</div>
+    );
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { HomePage } from "./HomePage/HomePage";
 export { Publicboard } from "./Publicboard/Publicboard";
 export { MainDashboard } from "./MainDashboard/MainDashboard";
+export { NotFound } from "./NotFound/NotFound";
