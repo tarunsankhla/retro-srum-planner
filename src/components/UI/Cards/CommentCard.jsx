bec7cbf345e3b100c01b2393a3e890517e926b65
saurabhchirde <saurabh123.chirde@gmail.com>
2022-05-09 00:06:07 +0530

    update: comment card, fix: UI

--- src/components/UI/Cards/CommentCard.jsx
+++ src/components/UI/Cards/CommentCard.jsx
@@ -8,116 +8,161 @@ import { getProjectData } from "utils/boardService";
 import "./CommentCard.css";
 
 export const CommentCard = ({
-	color,
-	feedback,
-	project,
-	columnName,
-	setProject,
+  color,
+  feedback,
+  project,
+  columnName,
+  setProject,
 }) => {
-	const { userState } = useAuth();
-	const navigate = useNavigate();
+  const { userState } = useAuth();
+  const navigate = useNavigate();
 
-	const { userId } = useParams();
+  const { userId } = useParams();
 
-	const [flag, setFlag] = useState(false);
-	const [isEdit, setIsEdit] = useState(false);
+  const [flag, setFlag] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [showCommentBox, setShowCommentBox] = useState(false);
 
-	const toggleEdit = () => setIsEdit(s => !s)
+  const toggleEdit = () => setIsEdit((s) => !s);
 
-	const toggleFlag = () => {
-		setFlag((f) => !f);
-	};
+  const toggleFlag = () => {
+    setFlag((f) => !f);
+  };
 
-	const updateLikes = (e) => {
-		e.preventDefault();
+  const updateLikes = (e) => {
+    e.preventDefault();
 
-		const doctoupdate = doc(firestore, `users/${userId}`);
+    const doctoupdate = doc(firestore, `users/${userId}`);
 
-		let columnUpdate = {
-			...project[columnName],
-			feedbacks: [
-				...project[columnName].feedbacks.map((currfeedback) =>
-					currfeedback.id === feedback.id
-						? { ...currfeedback, likes: currfeedback.likes + 1 }
-						: currfeedback
-				),
-			],
-		};
+    let columnUpdate = {
+      ...project[columnName],
+      feedbacks: [
+        ...project[columnName].feedbacks.map((currfeedback) =>
+          currfeedback.id === feedback.id
+            ? { ...currfeedback, likes: currfeedback.likes + 1 }
+            : currfeedback
+        ),
+      ],
+    };
 
-		let updateObj = {
-			[project.key]: { ...project, [columnName]: columnUpdate },
-		};
+    let updateObj = {
+      [project.key]: { ...project, [columnName]: columnUpdate },
+    };
 
-		updateDoc(doctoupdate, updateObj)
-			.then((res) => {})
-			.catch((err) => {
-				console.log(err);
-			});
+    updateDoc(doctoupdate, updateObj)
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
 
-		setTimeout(() => {
-			toggleFlag();
-		}, 100);
-	};
+    setTimeout(() => {
+      toggleFlag();
+    }, 100);
+  };
 
-	const deleteComment = (e)=>{
-		e.preventDefault();
-		
-		if(userState.user.userId !== feedback.userId && userState.user.userId !== userId){
-			alert("you are not valid to enter");
-			return
-		  }
+  const deleteComment = (e) => {
+    e.preventDefault();
 
-		const doctoupdate = doc(firestore, `users/${userId}`);
-		let columnUpdate = {
-			...project[columnName],
-			feedbacks: [
-				...project[columnName].feedbacks.filter((currfeedback) =>
-					currfeedback.id !== feedback.id
-				),
-			],
-		};
+    if (
+      userState.user.userId !== feedback.userId &&
+      userState.user.userId !== userId
+    ) {
+      alert("you are not valid to enter");
+      return;
+    }
 
-		let updateObj = {
-			[project.key]: { ...project, [columnName]: columnUpdate },
-		};
+    const doctoupdate = doc(firestore, `users/${userId}`);
+    let columnUpdate = {
+      ...project[columnName],
+      feedbacks: [
+        ...project[columnName].feedbacks.filter(
+          (currfeedback) => currfeedback.id !== feedback.id
+        ),
+      ],
+    };
 
-		updateDoc(doctoupdate, updateObj)
-			.then((res) => {})
-			.catch((err) => {
-				console.log(err);
-			});
+    let updateObj = {
+      [project.key]: { ...project, [columnName]: columnUpdate },
+    };
 
-		setTimeout(() => {
-			toggleFlag();
-		}, 200);
-	}
+    updateDoc(doctoupdate, updateObj)
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
 
-	const editComment = (e) =>{
-		setIsEdit(s => !s)
-	}
+    setTimeout(() => {
+      toggleFlag();
+    }, 200);
+  };
 
-	useEffect(() => {
-		getProjectData(setProject, userId, project.id, userState, navigate);
-	}, [flag,isEdit]);
+  const editComment = (e) => {
+    setIsEdit((s) => !s);
+  };
 
-	return (
-		<div className={`comment-card-wrapper ${color}`}>
-			<i className="fas fa-ellipsis-v card-option-menu" onClick={editComment}></i>
-			<p className="comment-card-text">{feedback?.textField}</p>
-			<div className="comment-card-buttons">
-				{feedback.likes}
-				<i onClick={updateLikes} className="far fa-thumbs-up thumb"></i>
-				<i className="far fa-comment comment"></i>
-				<i class="fas fa-trash" onClick={deleteComment}></i>
-			</div>
-			{isEdit && <AddFeedback
-			toggleModal={toggleEdit}
-			columnName={project[columnName].name}
-			userId={userId}
-			project={project}
-			isEdit={isEdit}
-			feedbackObj={feedback}
-			setProject={setProject}/>}
-		</div>
-	);
+  const addNewCommentHandler = (e) => {
+    setShowCommentBox(true);
+  };
+
+  const commentSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    getProjectData(setProject, userId, project.id, userState, navigate);
+  }, [flag, isEdit]);
+
+  return (
+    <div className={`comment-card-wrapper ${color}`}>
+      <i class="far fa-trash-alt trash" onClick={deleteComment}></i>
+      <p className="comment-card-text">{feedback?.textField}</p>
+      {showCommentBox && (
+        <form
+          onSubmit={(e) => commentSubmitHandler(e)}
+          className="comment-card-comments"
+        >
+          <textarea
+            className="comment-textarea"
+            type="text"
+            name="text"
+            placeholder="comments"
+          />
+          <div className="comment-cta-buttons">
+            <button
+              onClick={() => setShowCommentBox(false)}
+              className="btn secondary-outline-btn-sm"
+            >
+              Cancel
+            </button>
+            <button type="submit" className="btn primary-btn-sm">
+              Post
+            </button>
+          </div>
+        </form>
+      )}
+
+      <div className="comment-card-buttons">
+        {feedback.likes}
+        <i onClick={updateLikes} className="far fa-thumbs-up thumb"></i>
+        <i
+          onClick={(e) => {
+            addNewCommentHandler(e);
+          }}
+          className="far fa-comment comment"
+        ></i>
+        <i className="fas fa-pen pen-edit" onClick={editComment}></i>
+      </div>
+      {isEdit && (
+        <AddFeedback
+          toggleModal={toggleEdit}
+          columnName={project[columnName].name}
+          userId={userId}
+          project={project}
+          isEdit={isEdit}
+          feedbackObj={feedback}
+          setProject={setProject}
+        />
+      )}
+    </div>
+  );
 };
